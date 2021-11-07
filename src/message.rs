4b392beb9f864fe6f5f035252cede6f7ea09981d
setnorth <thorsten.schilling@gmail.com>
2021-11-07 20:54:37 +0100

    Working. Lifetime questions.

--- src/message.rs
+++ src/message.rs
@@ -10,48 +10,51 @@
 //! needs to be implemented. 
 use crate::state::State;
 use crate::types::*;
+use crate::yd_raw::YDRaw;
 
 use std::f64::consts::PI;
+use std::cmp;
 
-/// Messages can update the state and must contain data
-pub trait Message : MessageData{
-    fn update(&self, s: &mut State);
+
+/// NMEA2000 Raw format
+pub trait N2kRaw {
+    fn timestamp(&self) -> Timestamp;
+    fn src(&self) -> TSrc;
+    fn dest(&self) -> TDest;
+    fn prio(&self) -> TPrio;
+    fn pgn(&self) -> TPgn;
+    fn data(&self) -> TData;
 }
 
-/// Properties of a message
-pub trait MessageData{
-    /// Byte length
-    fn bytes(&self) -> usize;
-    /// Parameter group
-    fn pgn(&self) -> TPgn;
-    /// Indicator if they are "fast" messages
-    fn fast(&self) -> bool;
-    /// Check if they are complete
-    fn complete(&self) -> bool;
+impl N2kRaw for YDRaw{
+    fn timestamp(&self) -> Timestamp { self.timestamp }
+    fn src(&self) -> TSrc { self.src }
+    fn dest(&self) -> TDest { self.dest }
+    fn prio(&self) -> TPrio { self.prio }
+    fn pgn(&self) -> TPgn { self.pgn }
+    fn data(&self) -> TData { self.data.to_vec() }
+}
+
+pub enum N2kMessageErr{
+    /// The supplied raw packet is out of sequence
+    OutOfSequence,
+    /// The supplied raw packet is of unexpected length
+    UnexpectedLength
+}
+
+pub trait N2kFromRaw<T: N2kRaw + ?Sized>{
+    /// Returns true if the message is complete.
+    fn is_complete(&self) -> bool;
     
-    fn timestamp(&self) -> Timestamp;
-    fn timestamp_mut(&mut self) -> &mut Timestamp;
+    /// Parses message from a N2kRaw type.
+    /// 
+    /// Returns `()` if all data was copied an `N2kMessageErr` in case of a parsing error.
+    fn from_raw(&mut self, raw: &T) -> Result<(),N2kMessageErr>;
+}
 
-    fn prio(&self) -> TPrio;
-    fn prio_mut(&mut self) -> &mut TPrio;
-
-    fn src(&self) -> TSrc;
-    fn src_mut(&mut self) -> &mut TSrc;
-
-    fn dest(&self) -> TDest;
-    fn dest_mut(&mut self) -> &mut TDest;
-    
-    fn data(&self) -> &TData;
-    fn data_mut(&mut self) -> &mut TData;
-
-    fn counter_mask(&self) -> u8;
-    fn counter_mask_mut(&mut self) -> &mut u8;
-
-    fn next_packet(&self) -> u8;
-    fn next_packet_mut(&mut self) -> &mut u8;
-    
-    fn remaining_bytes(&self) -> usize;
-    fn remaining_bytes_mut(&mut self) -> &mut usize;
+/// Messages can update the state and must contain data
+pub trait N2kMessage : N2kFromRaw<dyn N2kRaw>{
+    fn update(&self, s: &mut State);
 }
 
 //*****************************************************************************
@@ -63,8 +66,6 @@ macro_rules! message_type {
     ($type_name: ident, $pgn: expr, $bytes: expr, $fast: expr) => {
         #[derive(Default)]
         pub struct $type_name {
-            //p: Packet,
-
             /// Time of the Message
             pub timestamp: Timestamp,
             /// Priority
@@ -83,6 +84,7 @@ macro_rules! message_type {
             /// Remaining bytes until the Message is complete
             pub remaining_bytes : usize,               
         }
+        
         impl $type_name{
             pub const PGN: TPgn = $pgn;
             pub const BYTES: usize = $bytes;
@@ -90,41 +92,76 @@ macro_rules! message_type {
 
             pub fn new() -> Self{ $type_name{..Default::default()} }
         }
-        impl MessageData for $type_name{
-            fn bytes(&self) -> usize{ $type_name::BYTES }
-            fn pgn(&self) -> TPgn{ $type_name::PGN }
-            fn fast(&self) -> bool{ $type_name::FAST }
-            fn complete(&self) -> bool{ self.remaining_bytes == 0 }
+        
+        //TODO: Down from here we can implement with impl<T: N2kRaw>
+        impl N2kFromRaw<dyn N2kRaw> for $type_name{
+            fn is_complete(&self) -> bool {
+                match self.remaining_bytes{
+                    0 => true,
+                    _ => false
+                }
+            }
 
-            fn timestamp(&self) -> Timestamp{ self.timestamp }
-            fn timestamp_mut(&mut self) -> &mut Timestamp { &mut self.timestamp }
-        
-            fn prio(&self) -> TPrio { self.prio }
-            fn prio_mut(&mut self) -> &mut TPrio { &mut self.prio }
-        
-            fn src(&self) -> TSrc { self.src }
-            fn src_mut(&mut self) -> &mut TSrc { &mut self.src }
-        
-            fn dest(&self) -> TDest { self.dest }
-            fn dest_mut(&mut self) -> &mut TDest { &mut self.dest }
-            
-            fn data(&self) -> &TData { &self.data }
-            fn data_mut(&mut self) -> &mut TData { &mut self.data }
-        
-            fn counter_mask(&self) -> u8 { self.counter_mask }
-            fn counter_mask_mut(&mut self) -> &mut u8 {&mut self.counter_mask }
-        
-            fn next_packet(&self) -> u8 { self.next_packet }
-            fn next_packet_mut(&mut self) -> &mut u8 { &mut self.next_packet }
-            
-            fn remaining_bytes(&self) -> usize { self.remaining_bytes }
-            fn remaining_bytes_mut(&mut self) -> &mut usize { &mut self.remaining_bytes }
+            fn from_raw(&mut self, raw: &dyn N2kRaw) -> Result<(),N2kMessageErr>{
+                //Is this a fast message?
+                if $type_name::FAST {
+                    //If we are just starting this new fast package
+                    if (self.next_packet == 0) && (raw.data()[0] & 0x1F == 0){
+                        //Check if this packet has the same length as we expect to see
+                        if $type_name::BYTES != raw.data()[1] as usize {
+                            return Err(N2kMessageErr::UnexpectedLength);
+                        }
+                        //Set values and the first 6 bytes for this package
+                        self.timestamp = raw.timestamp();
+                        self.src = raw.src();
+                        self.dest = raw.dest();
+                        self.prio = raw.prio();
+                        self.counter_mask = raw.data()[0];
+                        self.next_packet += 1;
+                        self.remaining_bytes = $type_name::BYTES - cmp::min($type_name::BYTES,6);
+                        self.data.append(&mut raw.data()[2..8_usize].to_vec());
+                    } else {
+                        //This packet is already begun...
+                        //If the packet is the next in series
+                        if self.next_packet == (self.counter_mask ^ raw.data()[0]){
+                            self.data.append(&mut raw.data()[1..cmp::min(self.remaining_bytes+1,8) as usize].to_vec());
+                            self.remaining_bytes -= cmp::min(self.remaining_bytes,7);
+                            self.next_packet += 1;
+                        } else {
+                            //It seems that the previous sequence was not finished. Try to start a new sequence.
+                            //Check that only bits in sequence identifier (raw.data[0] & 0b00011111) and sequence
+                            //size with what we expect.
+                            if ((raw.data()[0] & 0x1F == 0) && ((raw.data()[1] as usize ) == $type_name::BYTES as usize)){
+                                self.timestamp = raw.timestamp();
+                                self.src = raw.src();
+                                self.dest = raw.dest();
+                                self.prio = raw.prio();
+                                self.counter_mask = raw.data()[0];
+                                self.next_packet += 1;
+                                self.remaining_bytes = $type_name::BYTES - cmp::min($type_name::BYTES,6);
+                                self.data.clear();
+                                self.data.append(&mut raw.data()[2..8_usize].to_vec());
+                            } else {
+                                return Err(N2kMessageErr::OutOfSequence);
+                            }
+                        }
+                    }
+                } else {
+                    //Just a normal packet
+                    self.timestamp = raw.timestamp();
+                    self.src = raw.src();
+                    self.dest = raw.dest();
+                    self.prio = raw.prio();
+                    self.data.append(&mut raw.data().to_vec());
+                }
+                Ok(())
+            }
         }
     }
 }
 
 message_type!(WindMessage, 130306, 8, false);
-impl Message for WindMessage{
+impl N2kMessage for WindMessage{
     fn update(&self, s: &mut State){
         s.timestamp = self.timestamp;
         s.aws = u16::from_le_bytes([self.data[1],self.data[2]]) as f32 * 0.01 * 1.943_844_6; //in knots;
@@ -133,7 +170,7 @@ impl Message for WindMessage{
 }
 
 message_type!(PositionRapidUpdateMessage, 129025, 8, false);
-impl Message for PositionRapidUpdateMessage{
+impl N2kMessage for PositionRapidUpdateMessage{
     fn update(&self, s: &mut State){
         s.timestamp = self.timestamp;
         let mut lat = i32::from_le_bytes([  
@@ -155,7 +192,7 @@ impl Message for PositionRapidUpdateMessage{
 }
 
 message_type!(GNSSPositionData, 129029, 43, true);
-impl Message for GNSSPositionData{
+impl N2kMessage for GNSSPositionData{
     fn update(&self, s : &mut State){
         s.timestamp = self.timestamp;
         //Latitude
@@ -186,7 +223,7 @@ impl Message for GNSSPositionData{
 }
 
 message_type!(VesselHeadingMessage, 127250, 8, false);
-impl Message for VesselHeadingMessage{
+impl N2kMessage for VesselHeadingMessage{
     fn update(&self, s: &mut State){
         s.timestamp = self.timestamp;
         s.hdg = u16::from_le_bytes([self.data[1],self.data[2]]) as f32 * 0.0001 * 360.0 / 2.0 / PI as f32;
@@ -194,7 +231,7 @@ impl Message for VesselHeadingMessage{
 }
 
 message_type!(CogSogRapidUpdateMessage, 129026, 8, false);
-impl Message for CogSogRapidUpdateMessage{
+impl N2kMessage for CogSogRapidUpdateMessage{
     fn update(&self, s: &mut State){
         s.timestamp = self.timestamp;
         s.cog = u16::from_le_bytes([self.data[2],self.data[3]]) as f32 * 0.0001 * 360.0 / 2.0 / PI as f32;
@@ -203,7 +240,7 @@ impl Message for CogSogRapidUpdateMessage{
 }
 
 message_type!(SpeedMessage, 128259, 8, false);
-impl Message for SpeedMessage{
+impl N2kMessage for SpeedMessage{
     fn update(&self, s: &mut State){
         s.timestamp = self.timestamp;
         s.stw = u16::from_le_bytes([self.data[1],self.data[2]]) as f32 * 0.01 * 1.943_844_6; //in knots
@@ -211,7 +248,7 @@ impl Message for SpeedMessage{
 }
 
 message_type!(RateOfTurnMessage, 127251, 5, false);
-impl Message for RateOfTurnMessage{
+impl N2kMessage for RateOfTurnMessage{
     fn update(&self, s: &mut State){
         s.timestamp = self.timestamp;
         s.rot = i32::from_le_bytes([self.data[1],
@@ -222,7 +259,7 @@ impl Message for RateOfTurnMessage{
 }
 
 message_type!(AttitudeMessage, 127257, 7, false);
-impl Message for AttitudeMessage{
+impl N2kMessage for AttitudeMessage{
     fn update(&self, s: &mut State){
         s.timestamp = self.timestamp;
         s.yaw = i16::from_le_bytes([self.data[1],self.data[2]]) as f32 * 0.0001 * 360.0 / 2.0 / PI as f32;
@@ -232,7 +269,7 @@ impl Message for AttitudeMessage{
 }
 
 message_type!(RudderMessage, 127245, 8, false);
-impl Message for RudderMessage{
+impl N2kMessage for RudderMessage{
     fn update(&self, s: &mut State){
         s.timestamp = self.timestamp;
         let value = i16::from_le_bytes([self.data[4],self.data[5]]) as f32 * 0.0001;
